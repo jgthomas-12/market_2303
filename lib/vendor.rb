7db883b8dd26c8897441693268039dd91f159c1d
Jesse Thomas <jgthomas3@gmail.com>
2023-04-17 10:30:21 -0600

    Feat: Add Vendor class and 2 methods.

---
+++ lib/vendor.rb
@@ -0,0 +1,26 @@
+class Vendor
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    if @inventory.include?(item)
+      @inventory[item] 
+    else
+      0
+    end
+  end
+
+  def stock(item, amount)
+    if @inventory.include?(item)
+      @inventory[item] += amount
+    else
+      @inventory[item] = amount
+    end
+  end
+
+end
