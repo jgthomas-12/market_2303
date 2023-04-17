5ddaa2f4fed0b957073c824bfe7e3ff4f952214c
Jesse Thomas <jgthomas3@gmail.com>
2023-04-17 10:30:01 -0600

    Test: Add initialize.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,26 @@
 require 'rspec'
+require './lib/item'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: "Peach", price: "$0.75"})
+    @item2 = Item.new({name: "Tomato", price: "$0.50"})
+  end
+
+  describe "initialize" do
+    it "exists" do
+      expect(@item1).to be_an(Item)
+    end
+
+    it "has readable attributes" do
+      expect(@item1.name).to eq("Peach")
+      expect(@item1.price).to eq("$0.75")
+      expect(@item2.name).to eq("Tomato")
+      expect(@item2.price).to eq("$0.50")
+    end
+
+  end
+
+
+
+end
